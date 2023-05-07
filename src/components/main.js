a0aa70f851316a48e3df41f9e17ded610ac0096d
JustinStar-py <JustinStar.py@gmail.com>
2023-05-07 22:05:45 +0430

    New UI/U

---
+++ src/components/main.js
@@ -0,0 +1,35 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+
+function Main() {
+    return (<><div id="Top-Title-Div" >
+    <h1 id="Top-Title">AI NFT <br></br>DApp Generator <br></br> 
+         <GitHubIcon fontSize="large" />
+         <TelegramIcon fontSize="large" />
+         <TwitterIcon fontSize="large" />
+     </h1>
+    <div>
+    </div>
+    </div>
+    <div  id="Top-Title-Div-Right">
+        <h1 id="Top-Title-Right">Cheap & Free<br></br>Lets make it toghater</h1>
+    </div>
+    <div id="Top-Title-Div-Three" >
+        <h1 id="Top-Title">Check The<br></br>Github Repository</h1>
+    </div>
+    <div className='mainWindow'>
+        <div className='tradeBox-two'>
+            <h1>Try launch DApp</h1>
+            <p>Users can make they images and they if they want they can make it to an NFT by cheap fee.</p>
+            <p>The App powered by React, Node.js and libraies we include: Mui as front-end library, Web3, Hardhat</p>
+        <div className="rightH">
+        <div className='connectButton'>Launch DApp</div>
+    </div>
+    </div>
+    </div></>
+)
+}
+
+export default Main
